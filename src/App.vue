32e7712a79bc9a3f4ca1d84c0e993309188f931d
CaptainSquib1 <alistairbegging@gmail.com>
2026-02-04 09:39:38 -0600

    troubleshooting npm

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref, computed} from 'vue'
+import home from './home.vue'
+import about from './about.vue'
+
+const routes = {
+  '/': home,
+  '/about': about
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+
+</script>
+
+
+<template>
+  <a href="#/">Home</a> |
+  <a href="#/about">About</a>
+
+  <component :is="currentView"></component>
+
+
+  <v-icon icon = "mdi-home"></v-icon>
+</template>
+
+
+
