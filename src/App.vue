a35d9b402d6362702d021b575070802c567fb757
CaptainSquib1 <alistairbegging@gmail.com>
2026-02-06 20:42:55 -0600

    Updating Site

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
-<script setup></script>
+<script setup>
+import {ref, computed} from 'vue'
+import home from './home.vue'
+import about from './about.vue'
+
+const routes = {
+  '/': home,
+  '/about': about
+}
+
+const currentPath = ref(window.location.hash)
+const drawer = ref(false);
+const rail = ref(false);
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" :rail="rail"
+                         permanent
+                         @click="rail = false">
+      <v-list-item prepend-icon="mdi-home" href="#/" title="Home" ></v-list-item>
+      <v-list-item prepend-icon="mdi-information-variant-circle-outline" href="#/about" title="About Us" ></v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar color="orange-lighten-3">
+      <v-app-bar-nav-icon @click = "drawer = !drawer" icon="mdi-hamburger"></v-app-bar-nav-icon>
+      <v-app-bar-title>Book Stand</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <component :is="currentView"></component>
+
+    </v-main>
+    <v-footer app="true">Copyright 2026</v-footer>
+
+  </v-app>
 </template>
 
-<style scoped></style>
+
+
